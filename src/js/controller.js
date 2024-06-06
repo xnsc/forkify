794f3676b7033a761a82849b8d670f1141ec0c65
Nicole Chen <nschen01@gmail.com>
2024-06-06 11:26:29 -0700

    Add loading of diff recipes

--- src/js/controller.js
+++ src/js/controller.js
@@ -29,12 +29,18 @@ const renderSpinner = function (parentEl) {
   parentEl.insertAdjacentHTML('afterbegin', markup);
 };
 
-const showRecipes = async function () {
+const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) {
+      return;
+    }
+
     // 1.) Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
 
     const data = await res.json();
@@ -164,4 +170,7 @@ const showRecipes = async function () {
   }
 };
 
-showRecipes();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+
+window.addEventListener('hashchange', showRecipe);
+window.addEventListener('load', showRecipe);
