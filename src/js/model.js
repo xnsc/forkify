a869878de1254e8fb2941d9e2d500ccd63a422ee
Nicole Chen <nschen01@gmail.com>
2024-06-16 23:22:38 -0700

    Refactor to utilize globals

--- src/js/model.js
+++ src/js/model.js
@@ -1,20 +1,13 @@
+import { API_URL } from './config';
+import { getJSON } from './helpers';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-
-    const data = await res.json();
-
-    if (!res.ok) {
-      throw new Error(`${data.message} (${res.status})`);
-    }
-
-    console.log(res, data);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -30,6 +23,6 @@ export const loadRecipe = async function (id) {
 
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    console.error(err);
   }
 };
